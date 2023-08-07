446608c2ad5502e0a970a575ae911ec2113b02ce
DESKTOP-JMLCGCK\HP <younkyong94@gmail.com>
2023-08-07 17:55:26 +0900

    2023.08.07

---
+++ src/ch9/q1.java
@@ -0,0 +1,19 @@
+package ch9;
+
+import java.util.Scanner;
+
+public class q1 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = in.nextInt();
+        }
+
+
+    }
+}
